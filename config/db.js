e17cc498cb418ad8ac689355c550c2e208e12f80
lautarocloudy <119749975+lautarocloudy@users.noreply.github.com>
2025-10-19 20:58:00 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,8 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
-    console.log("MONGO_URI:", process.env.uri);
+
 await mongoose.connect("mongodb+srv://logy:123@cluster0.pegtvdi.mongodb.net/logy-manager?retryWrites=true&w=majority");
     console.log('✅ MongoDB conectado correctamente');
   } catch (error) {
