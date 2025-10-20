41d49788170ff30bfc25455c532f44702cf8f14d
lautarocloudy <119749975+lautarocloudy@users.noreply.github.com>
2025-10-19 21:10:14 -0300

    Delete models/Archivo.js

--- models/Archivo.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const archivoSchema = new mongoose.Schema({
-  filename: String,
-  originalName: String,
-  size: Number,
-  url: String,
-  uploadedAt: { type: Date, default: Date.now }
-});
-export default mongoose.model('Archivo', archivoSchema);
